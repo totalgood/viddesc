f7a368b0ee06807fd4b670f3d3546a958a84a8ed
Natural Language Processing in Action <nlpia@totalgood.com>
2018-07-18 09:36:56 -0700

    startLine numLines and dataSet args are all integers dataSet=0=training set

--- download_verbose.sh
+++ download_verbose.sh
@@ -5,17 +5,29 @@ E_BADARGS=65
 
 if [ $# -lt $EXPECTED_ARGS ]
 then
-  echo "Usage: `basename $0` <username-MPIIMD> <password-MPIIMD> [parallelDownloads=8] [skipLines=40459]"
+  echo "Usage: `basename $0` <username-MPIIMD> <password-MPIIMD> [parallelDownloads=8] [firstLine=1] [numLines=1000000000]"
   exit $E_BADARGS
 fi
 usernameMD=$1
 passwordMD=$2
 parallelDownloads=$3
-skipLines=$4
+firstLine=$4
+numLines=$5
+dataSet=$6
+
+if [ $# -lt 6 ]
+then
+  dataSet=0  # training set
+fi
+
+if [ $# -lt 5 ]
+then
+  numLines=1000000000  # 1 billion videos+descriptions
+fi
 
 if [ $# -lt 4 ]
 then
-  skipLines=21650
+  firstLine=1  # 21650
 fi
 
 if [ $# -lt 3 ]
@@ -38,28 +50,40 @@ fi
 ########## download annotations
 # Training set
 wget -c http://datasets.d2.mpi-inf.mpg.de/movieDescription/protected/lsmdc2016/LSMDC16_annos_training.csv --user=$usernameMD --password=$passwordMD
+
 # Validation set
 wget -c http://datasets.d2.mpi-inf.mpg.de/movieDescription/protected/lsmdc2016/LSMDC16_annos_val.csv --user=$usernameMD --password=$passwordMD
+
 # Public_Test set
 wget -c http://datasets.d2.mpi-inf.mpg.de/movieDescription/protected/lsmdc2016/LSMDC16_annos_test.csv --user=$usernameMD --password=$passwordMD
+
 # Blind_Test set
 wget -c http://datasets.d2.mpi-inf.mpg.de/movieDescription/protected/lsmdc2016/LSMDC16_annos_blindtest.csv --user=$usernameMD --password=$passwordMD
 
 ########## download videos
 # avi clips from MPII-MD
-filesToDownload="MPIIMD_downloadLinks.txt"
-wget -c http://datasets.d2.mpi-inf.mpg.de/movieDescription/protected/lsmdc2016/"$filesToDownload" --user=$usernameMD --password=$passwordMD
-cat $filesToDownload | wc
-cat $filesToDownload | tail -n +$skipLines | xargs "${FLAGS[@]}" wget -crnH -q --show-progress=on --cut-dirs=3 --user=$usernameMD --password=$passwordMD
+if [ $# -lt 6 ] || [ "$dataSet" -eq "0" ]
+then
+  filesToDownload="MPIIMD_downloadLinks.txt"
+  wget -c http://datasets.d2.mpi-inf.mpg.de/movieDescription/protected/lsmdc2016/"$filesToDownload" --user=$usernameMD --password=$passwordMD
+  cat $filesToDownload | wc
+  cat $filesToDownload | tail -n +$firstLine | head -n $numLines | xargs "${FLAGS[@]}" wget -crnH -q --show-progress=on --cut-dirs=3 --user=$usernameMD --password=$passwordMD
+fi
 
 # avi clips from M-VAD-test-aligned
-filesToDownloadT="MVADaligned_downloadLinks.txt"
-wget -c http://datasets.d2.mpi-inf.mpg.de/movieDescription/protected/lsmdc2016/"$filesToDownload" --user=$usernameMD --password=$passwordMD
-cat $filesToDownload | wc
-cat $filesToDownload | xargs "${FLAGS[@]}" wget -crnH -o $filesToDownload.log --cut-dirs=3 --user=$usernameMD --password=$passwordMD
+if [ $# -lt 6 ] || [ "$dataSet" -eq "1" ]
+then
+  filesToDownloadT="MVADaligned_downloadLinks.txt"
+  wget -c http://datasets.d2.mpi-inf.mpg.de/movieDescription/protected/lsmdc2016/"$filesToDownload" --user=$usernameMD --password=$passwordMD
+  cat $filesToDownload | wc
+  cat $filesToDownload | tail -n +$firstLine | head -n $numLines | xargs "${FLAGS[@]}" wget -crnH -q --show-progress=on --cut-dirs=3 --user=$usernameMD --password=$passwordMD
+fi
 
 # avi clips from Blind Test set
-filesToDownload="BlindTest_downloadLinks.txt"
-wget -c http://datasets.d2.mpi-inf.mpg.de/movieDescription/protected/lsmdc2016/"$filesToDownload" --user=$usernameMD --password=$passwordMD
-cat $filesToDownload | wc
-cat $filesToDownload | xargs "${FLAGS[@]}" wget -crnH -o $filesToDownload.log --cut-dirs=3 --user=$usernameMD --password=$passwordMD
+if [ $# -lt 6 ] || [ "$dataSet" -eq "2" ]
+then
+  filesToDownload="BlindTest_downloadLinks.txt"
+  wget -c http://datasets.d2.mpi-inf.mpg.de/movieDescription/protected/lsmdc2016/"$filesToDownload" --user=$usernameMD --password=$passwordMD
+  cat $filesToDownload | wc
+  cat $filesToDownload | tail -n +$firstLine | head -n $numLines | xargs "${FLAGS[@]}" wget -crnH -o $filesToDownload.log --cut-dirs=3 --user=$usernameMD --password=$passwordMD
+fi
