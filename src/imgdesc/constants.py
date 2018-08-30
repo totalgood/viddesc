3c21bbfc6717343b43b56c741590efb39ad68163
Hobson Lane <github@totalgood.com>
2018-08-30 11:08:33 -0700

    constants.py

--- src/imgdesc/constants.py
+++ src/imgdesc/constants.py
@@ -1,3 +1,4 @@
 #!/python
 
-BASE_DIR = '/midata/imdesc'
+BASE_DIR = os.path.abspath(os.path.join(os.path.normpath('/midata'), 'imdesc'))
+MODELS_DIR = os.path.abspath(os.path.join(BASE_DIR, 'models'))
--- src/imgdesc/sample.py
+++ src/imgdesc/sample.py
@@ -10,17 +10,21 @@ from model import EncoderCNN, DecoderRNN
 from PIL import Image
 
 
+from constants import BASE_DIR, MODELS_DIR
+
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 def load_image(image_path, transform=None):
     image = Image.open(image_path)
     image = image.resize([224, 224], Image.LANCZOS)
-    
+
     if transform is not None:
         image = transform(image).unsqueeze(0)
-    
+
     return image
+
 
 def main(args):
     # Image preprocessing
@@ -28,13 +32,14 @@ def main(args):
         transforms.ToTensor(), 
         transforms.Normalize((0.485, 0.456, 0.406), 
                              (0.229, 0.224, 0.225))])
-    
+
     # Load vocabulary wrapper
     with open(args.vocab_path, 'rb') as f:
         vocab = pickle.load(f)
 
     # Build models
-    encoder = EncoderCNN(args.embed_size).eval()  # eval mode (batchnorm uses moving mean/variance)
+    encoder = EncoderCNN(args.embed_size).eval()
+    # eval mode (batchnorm uses moving mean/variance)
     decoder = DecoderRNN(args.embed_size, args.hidden_size, len(vocab), args.num_layers)
     encoder = encoder.to(device)
     decoder = decoder.to(device)
@@ -46,12 +51,12 @@ def main(args):
     # Prepare an image
     image = load_image(args.image, transform)
     image_tensor = image.to(device)
-    
+
     # Generate an caption from the image
     feature = encoder(image_tensor)
     sampled_ids = decoder.sample(feature)
     sampled_ids = sampled_ids[0].cpu().numpy()          # (1, max_seq_length) -> (max_seq_length)
-    
+
     # Convert word_ids to words
     sampled_caption = []
     for word_id in sampled_ids:
@@ -60,22 +65,28 @@ def main(args):
         if word == '<end>':
             break
     sentence = ' '.join(sampled_caption)
-    
+
     # Print out the image and the generated caption
-    print (sentence)
+    print(sentence)
     image = Image.open(args.image)
     plt.imshow(np.asarray(image))
-    
+
+
+def default_args(args):
+    latest_decoder_path = os.list_dir(MODELS_DIR)
+    if not args.decoder_path:
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--image', type=str, required=True, help='input image for generating caption')
-    parser.add_argument('--encoder_path', type=str, default='models/encoder-2-1000.ckpt', help='path for trained encoder')
+    parser.add_argument('--encoder_path', type=str, default='', help='path for trained encoder')
     parser.add_argument('--decoder_path', type=str, default='models/decoder-2-1000.ckpt', help='path for trained decoder')
     parser.add_argument('--vocab_path', type=str, default='data/vocab.pkl', help='path for vocabulary wrapper')
-    
+
     # Model parameters (should be same as paramters in train.py)
-    parser.add_argument('--embed_size', type=int , default=256, help='dimension of word embedding vectors')
-    parser.add_argument('--hidden_size', type=int , default=512, help='dimension of lstm hidden states')
-    parser.add_argument('--num_layers', type=int , default=1, help='number of layers in lstm')
+    parser.add_argument('--embed_size', type=int, default=256, help='dimension of word embedding vectors')
+    parser.add_argument('--hidden_size', type=int, default=512, help='dimension of lstm hidden states')
+    parser.add_argument('--num_layers', type=int, default=1, help='number of layers in lstm')
     args = parser.parse_args()
     main(args)
